e67ba9e72613180c1adfaf7fed694933a65c999c
Aniket Kale <aniket@geospoc.com>
2022-08-09 15:20:26 +0530

    feat: crud api

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  students = [];
+
+  getStudents() {
+    return this.students;
+  }
+
+  getSpecificStudent(index) {
+    return this.students[index];
+  }
+
+  createStudent(student) {
+    this.students.push(student);
+    return student;
+  }
+
+  deleteStudent(index) {
+    return this.students.splice(index, 1);
+  }
+
+  updateStudent(id, student) {
+    return (this.students[id] = student);
+  }
+}
