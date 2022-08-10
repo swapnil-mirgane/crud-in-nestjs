32685410f751938815d920f4ec0397757be04738
Swapnil Mirgane <swapnil.mirgane@corp.ola.com>
2022-08-10 12:13:26 +0530

    GPG key verified tag

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -32,7 +32,7 @@ export class StudentController {
 
   @Delete(':id')
   deleteStudent(@Param() id) {
-    return this.studentService.deleteStudent(id);
+    return this.studentService.deleteStudent(id.id);
   }
 
   @Patch(':id')
